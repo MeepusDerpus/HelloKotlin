da4d8cdec4789a8b8e4ffed334c903043590648a
MeepusDerpus <ion@live.co.za>
2021-07-16 15:46:21 +0200

    Cleaned,
    Cleaned up and did a few examples

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,32 +1,45 @@
 fun buildAquarium() {
-    val myAquarium = Aquarium();
-    myAquarium.printSize();
+    val myAquarium = Aquarium()
+    myAquarium.printSize()
 }
 
 fun main() {
-   // buildAquarium();
+    buildAquarium()
 
-    val x = 1; // means x is final
-    var y = 2; //y can be reassigned
-    var z: Int = 3;
-    val S: String = "X is $x"; //just like php
-    var S1 = "X + Y is ${x + y}";
-    println(S + "\n" + S1);
+    val x = 1 // means x is final
+    var y = 2 //y can be reassigned
+    var z: Int = 3
+    val S: String = "X is $x" //just like php
+    var S1 = "X + Y is ${x + y}"
+    println(S + "\n" + S1)
 
     if (y in 1..100)
     {
-        println("Y $y is within 1 and 100");
+        println("Y $y is within 1 and 100")
     }
 
     //switch statement
-    println("Enter a Number:");
-    val i: Int = readLine()?.toIntOrNull() ?: 0; //returns 0 if null
-
+    print("Enter a Number:\n")
+    val i: Int = readLine()?.toInt() ?: 0 //returns 0 if null
+    //val i = readLine()!!.toInt();
+    println(i)
 
     when(i)
     {
-        0 -> println("$i is 0 or null");
-        in 1..50 -> println("$i is between 1 and 50");
-        else -> println("Above 50 or less than 0");
+        0 -> println("$i is 0 or null")
+        in 1..50 -> println("$i is between 1 and 50")
+        else -> println("$i is Above 50 or less than 0")
     }
+    //Kotlin Arrays are fixed size
+    val school1 = arrayOf("shark", "salmon", "minnow")
+    var school2 = arrayOf("shark", "salmon", "minnow")
+    val school3 = listOf("shark", "salmon", "minnow")
+    val school4 = mutableListOf("shark", "salmon", "minnow")
+
+    for (element in school1)
+    {
+        println(element)
+    }
+    for (i in 3..8 step 2) print(i)
+
 }
